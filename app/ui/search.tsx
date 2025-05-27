1fda85fc98967bc189889dadc8054989af3126b0
vermoudth <612210434@utsalamanca.edu.mx>
2025-05-26 19:51:28 -0600

    Correccion de errores

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,27 +1,25 @@
 'use client';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-    function handleSearch(term: string) {
-      const handleSearch = useDebouncedCallback((term) => {
-      console.log(`Searching... ${term}`);
-       const params = new URLSearchParams(searchParams);
-       const pathname = usePathname();
-       const { replace } = useRouter();
-       params.set('page', '1');
-       if (term) {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-}, 300);
-  }
-  
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -30,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-                onChange={(e) => {
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
@@ -39,3 +37,4 @@ export default function Search({ placeholder }: { placeholder: string }) {
     </div>
   );
 }
+  
